115d1f53a0b8f6801a2bfca5503072d171fcef5c
vjtolliver <vj.tolliver@gmail.com>
2023-03-30 20:10:25 -0700

    added first window prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,65 @@
 // Assignment code here
 // Generate Password length prompts
-var generate = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
-generate.addEventListener("click", askprompts);
+generateBtn.addEventListener("click", askprompts);
 
+ var lengthArr = [8, 9, 10, 11, 12, , 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 120, 121, 122, 123, 124, 125, 126, 127, 128]
+var lowercaseArr = []
+// Created first window prompt
 function askprompts() {
-  window.prompt("Enter a desired password length of 8 to 128 characters")
-} l
+var userChoice = window.prompt("Enter a desired password length of 8 to 128 characters")
+  if (userChoice > 7 && userChoice < 129) {
+    window.confirm("You chose " + userChoice + " characters.");
+  } else {
+    window.alert("Error: Unauthorized input. Please try again.");
+  }
+}
+
+function askLowercase() {
+  var userChoice = window.confirm("Do you want to include lowercase letters in your password?")
+  if (userChoice) {
+
+  }
+}
+//window.confirm("Do you want to include lowercase letters in your password?");
+//window.confirm("Do you want to include uppercase letters in your password?");
+//window.confirm("Do you want to include numbers in your password?");
+//window.confirm("Do you want to include special characters in your password");
+//}
+
+//var userInput = Math.random();
+// if (user)
+  // window.prompt("Enter a desired password length of 8 to 128 characters"); //
+// } 
+// var userInput = {
+  // min = 8
+  // max = 128
+// }
+//for (var i = 0; i < userValues.length; i++) {
+  //if (userValues[i] === userPrompt) {
+   //confirm("You chose" + userPrompt + "Do you want to include lowercase letters in your password?");
+  //}
+  // if else (userInput < 129) {
+    // window.prompt("You chose" + userInput + "Do you want to include lowercase letters in your password?");
+  // }
+//else (!userValues[i] === userPrompt) {
+  //return;
+//}
+//}
+
+// function askprompts ()
+// if (userInput === 8) {
+  // window.prompt("You chose" + userInput + "Do you want to include lowercase letters in your password")
+// } else (!userInput) {
+// return;
+// }
+
+
+
 
 // if (onclick.generate) {
-  // window.prompt("Enter a desired password length of 8 to 128 characters")
+  // window.prompt("Enter a desired password length of 8 to 128 characters")//
 // }
 
  
